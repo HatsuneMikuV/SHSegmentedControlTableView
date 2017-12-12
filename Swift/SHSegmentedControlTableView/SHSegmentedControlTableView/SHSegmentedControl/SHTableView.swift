0ec17d52f1477e55def651dd11ade284532d9204
Icarus <15188588180@163.com>
2017-12-12 17:25:50 +0800

    swift---增加相关类

---
+++ Swift/SHSegmentedControlTableView/SHSegmentedControlTableView/SHSegmentedControl/SHTableView.swift
@@ -0,0 +1,21 @@
+//
+//  SHTableView.swift
+//  SHSegmentedControlTableView
+//
+//  Created by angle on 2017/12/12.
+//  Copyright © 2017年 angle. All rights reserved.
+//
+
+import UIKit
+
+class SHTableView: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
