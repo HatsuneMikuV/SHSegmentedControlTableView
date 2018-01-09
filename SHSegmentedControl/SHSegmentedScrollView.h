3db34f53eb2a5150858973d7461413132584d7c7
Icarus <15188588180@163.com>
2018-01-09 16:15:58 +0800

    添加新的管理样式

---
+++ SHSegmentedControl/SHSegmentedScrollView.h
@@ -0,0 +1,18 @@
+//
+//  SHSegmentedScrollView.h
+//  SHSegmentedControlTableView
+//
+//  Created by angle on 2018/1/9.
+//  Copyright © 2018年 angle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <MJRefresh/MJRefresh.h>
+
+#import "SHSegmentControl.h"
+#import "SHCollectionView.h"
+
+@interface SHSegmentedScrollView : UIView
+
+@end
---
+++ SHSegmentedControl/SHSegmentedScrollView.m
@@ -0,0 +1,21 @@
+//
+//  SHSegmentedScrollView.m
+//  SHSegmentedControlTableView
+//
+//  Created by angle on 2018/1/9.
+//  Copyright © 2018年 angle. All rights reserved.
+//
+
+#import "SHSegmentedScrollView.h"
+
+@implementation SHSegmentedScrollView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
